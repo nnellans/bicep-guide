75541644bc58f5e87aaec510e0f34607e206bfd7
nnellans <54514037+nnellans@users.noreply.github.com>
2022-04-17 10:14:05 -0500

    Update az-cli.sh

--- deployment-options/az-cli.sh
+++ deployment-options/az-cli.sh
@@ -6,37 +6,37 @@ az deployment group create \
   --resource-group <rgName> \
   --subscription <subName> \                                                            # Optional. Default: currently logged in Subscription
   --mode Incremental | Complete \                                                       # Optional. Default: Incremental
-  --rollback-on-error | --rollback-on-error <name>                                      # Optional, choose one
-  --name <deploymentName>                                                               # Optional. Default: name of the Bicep file
-  --template-file <template.bicep>
-  --parameters <template.parameters.json>                                               # Optional
-  --parameters Key1='Value1' Key2='Value2'                                              # Optional
+  --rollback-on-error | --rollback-on-error <name> \                                    # Optional, choose one
+  --name <deploymentName> \                                                             # Optional. Default: name of the Bicep file
+  --template-file <template.bicep> \
+  --parameters <template.parameters.json> \                                             # Optional
+  --parameters Key1='Value1' Key2='Value2' \                                            # Optional
   --what-if | --confirm-with-what-if | --confirm-with-what-if --proceed-if-no-change    # Optional, choose one
 
 # Deploy Bicep to a Subscription
 az deployment sub create \
   --location <region> \
-  --name <deploymentName>                                                               # Optional. Default: name of the Bicep file
-  --template-file <template.bicep>
-  --parameters <template.parameters.json>                                               # Optional
-  --parameters Key1='Value1' Key2='Value2'                                              # Optional
+  --name <deploymentName> \                                                             # Optional. Default: name of the Bicep file
+  --template-file <template.bicep> \
+  --parameters <template.parameters.json> \                                             # Optional
+  --parameters Key1='Value1' Key2='Value2' \                                            # Optional
   --what-if | --confirm-with-what-if | --confirm-with-what-if --proceed-if-no-change    # Optional, choose one
 
 # Deploy Bicep to a Management Group
 az deployment mg create \
   --location <region> \
   --management-group-id <mgId> \
-  --name <deploymentName>                                                               # Optional. Default: name of the Bicep file
+  --name <deploymentName> \                                                             # Optional. Default: name of the Bicep file
   --template-file <template.bicep>
-  --parameters <template.parameters.json>                                               # Optional
-  --parameters Key1='Value1' Key2='Value2'                                              # Optional
+  --parameters <template.parameters.json> \                                             # Optional
+  --parameters Key1='Value1' Key2='Value2' \                                            # Optional
   --what-if | --confirm-with-what-if | --confirm-with-what-if --proceed-if-no-change    # Optional, choose one
 
 # Deploy Bicep to a Tenant
 az deployment tenant create \
   --location <region> \
-  --name <deploymentName>                                                               # Optional. Default: name of the Bicep file
-  --template-file <template.bicep>
-  --parameters <template.parameters.json>                                               # Optional
-  --parameters Key1='Value1' Key2='Value2'                                              # Optional
+  --name <deploymentName> \                                                             # Optional. Default: name of the Bicep file
+  --template-file <template.bicep> \
+  --parameters <template.parameters.json> \                                             # Optional
+  --parameters Key1='Value1' Key2='Value2' \                                            # Optional
   --what-if | --confirm-with-what-if | --confirm-with-what-if --proceed-if-no-change    # Optional, choose one
