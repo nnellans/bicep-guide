11510810bde3aff5eb1b30d06109629052c9253a
Nathan Nellans <54514037+nnellans@users.noreply.github.com>
2025-03-09 10:42:00 -0500

    Add deployment stack commands for sub and mg scopes

--- deployment-options/az-cli-stacks.sh
+++ deployment-options/az-cli-stacks.sh
@@ -3,22 +3,52 @@
 
 # Deployment Stacks at the Resource Group scope
 az stack group create \
-  --name <stackName> \                                          # aka -n
-  --resource-group <rgName> \                                   # aka -g
+  --name <stackName> \                                          # aka -n. name of the Stack
   --action-on-unmanage deleteAll|deleteResources|detachAll \    # aka --aou
   --deny-settings-mode denyDelete|denyWriteAndDelete|none \     # aka --dm
+  --resource-group <rgName> \                                   # aka -g. resource group to deploy the Stack to
   --template-file <main.bicep> \                                # aka -f
   --parameters <main.bicepparam> \                              # aka -p. Optional
   --parameters key1=value key2=value \                          # aka -p. Optional
   --bypass-stack-out-of-sync-error false|true \                 # aka --bse. Optional. Default: false
   --deny-settings-apply-to-child-scopes \                       # aka --cs. Optional
   --deny-settings-excluded-actions <actions> \                  # aka --ea. Optional
-  --deny-settings-excluded-principals <principalIDs> \          # aka --ed. Optional
-  --description <description> \                                 # Optional
+  --deny-settings-excluded-principals <principalIDs> \          # aka --ep. Optional
+  --description <description> \                                 # Optional. description of the Stack
   --tags <kvpairs>                                              # Optional
-  
 
 # Deployment Stacks at the Subscription scope
-
+az stack sub create \
+  --name <stackName> \                                          # aka -n. name of the Stack
+  --action-on-unmanage deleteAll|deleteResources|detachAll \    # aka --aou
+  --deny-settings-mode denyDelete|denyWriteAndDelete|none \     # aka --dm
+  --location <location> \                                       # aka -l. location to store Deployment Stack metadata
+  --subscription <subName> \                                    # optional. subscription to deploy the stack to. default: current subscription
+  --deployment-resource-group <rgName> \                        # aka --dr. optional. If deploying Stack to Sub and Bicep to RG
+  --template-file <main.bicep> \                                # aka -f
+  --parameters <main.bicepparam> \                              # aka -p. Optional
+  --parameters key1=value key2=value \                          # aka -p. Optional
+  --bypass-stack-out-of-sync-error false|true \                 # aka --bse. Optional. Default: false
+  --deny-settings-apply-to-child-scopes \                       # aka --cs. Optional
+  --deny-settings-excluded-actions <actions> \                  # aka --ea. Optional
+  --deny-settings-excluded-principals <principalIDs> \          # aka --ep. Optional
+  --description <description> \                                 # Optional. description of the Stack
+  --tags <kvpairs>                                              # Optional
 
 # Deployment Stacks at the Management Group scope
+az stack mg create \
+  --name <stackName> \                                          # aka -n. name of the Stack
+  --action-on-unmanage deleteAll|deleteResources|detachAll \    # aka --aou
+  --deny-settings-mode denyDelete|denyWriteAndDelete|none \     # aka --dm
+  --location <location> \                                       # aka -l. location to store Deployment Stack metadata
+  --management-group-id <mgId> \                                # aka -m. management group to deploy the stack to
+  --deployment-subscription <subName> \                         # aka --ds. optional. If deploying Stack to Mg and Bicep to Sub
+  --template-file <main.bicep> \                                # aka -f
+  --parameters <main.bicepparam> \                              # aka -p. Optional
+  --parameters key1=value key2=value \                          # aka -p. Optional
+  --bypass-stack-out-of-sync-error false|true \                 # aka --bse. Optional. default: false
+  --deny-settings-apply-to-child-scopes \                       # aka --cs. Optional
+  --deny-settings-excluded-actions <actions> \                  # aka --ea. Optional
+  --deny-settings-excluded-principals <principalIDs> \          # aka --ep. Optional
+  --description <description> \                                 # Optional. description of the Stack
+  --tags <kvpairs>                                              # Optional
