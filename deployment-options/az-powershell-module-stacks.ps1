7d90caa3691c6c57a64939e60da7d332622f0829
Nathan Nellans <54514037+nnellans@users.noreply.github.com>
2025-03-09 11:38:48 -0500

    Update az-powershell-module-stacks.ps1

--- deployment-options/az-powershell-module-stacks.ps1
+++ deployment-options/az-powershell-module-stacks.ps1
@@ -21,9 +21,37 @@ New-AzResourceGroupDeploymentStack `
   -Tag <tagHashTable>                                        # Optional
 
 # Deployment Stacks at the Subscription scope
+# Will deploy to currently selected Subscription, change with: Set-AzContext -Subscription <subName>
 New-AzSubscriptionDeploymentStack `
-
+  -Name <name> `                                             # name of the Stack
+  -ActionOnUnmanage deleteAll|deleteResources|detachAll `
+  -DenySettingsMode denyDelete|denyWriteAndDelete|none `
+  -Location <location> `                                     # location to store Deployment Stack metadata
+  -DeploymentResourceGroupName <rgName> `                    # Optional. If deploying Stack to Sub and Bicep to RG
+  -TemplateFile <main.bicep> `
+  -TemplateParameterFile <main.bicepparam> `                 # Optional
+  -TemplateParameterObject <paramsHashTable> `               # Optional
+  -BypassStackOutOfSyncError `                               # Optional
+  -DenySettingsApplyToChildScope `                           # Optional
+  -DenySettingsExcludedAction <actions> `                    # Optional
+  -DenySettingsExcludedPrincipal <principalIDs> `            # Optional
+  -Description <description> `                               # Optional. description of the Stack
+  -Tag <tagHashTable>                                        # Optional
 
 # Deployment Stacks at the Management Group scope
 New-AzManagementGroupDeploymentStack `
-
+  -Name <name> `                                             # name of the Stack
+  -ActionOnUnmanage deleteAll|deleteResources|detachAll `
+  -DenySettingsMode denyDelete|denyWriteAndDelete|none `
+  -Location <location> `                                     # location to store Deployment Stack metadata
+  -ManagementGroupId <mgId> `                                # management group to deploy stack to
+  -DeploymentSubscriptionId <subId> `                        # Optional. If deploying Stack to MG and Bicep to Sub
+  -TemplateFile <main.bicep> `
+  -TemplateParameterFile <main.bicepparam> `                 # Optional
+  -TemplateParameterObject <paramsHashTable> `               # Optional
+  -BypassStackOutOfSyncError `                               # Optional
+  -DenySettingsApplyToChildScope `                           # Optional
+  -DenySettingsExcludedAction <actions> `                    # Optional
+  -DenySettingsExcludedPrincipal <principalIDs> `            # Optional
+  -Description <description> `                               # Optional. description of the Stack
+  -Tag <tagHashTable>                                        # Optional
